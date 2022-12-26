ba86e409480b782b13c8cfc80c05366f279f2316
Andre Rangel <andreavila.rangel@gmail.com>
2022-12-26 18:28:46 -0300

    android/ios

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  printWidth: 80,
+  tabWidth: 2,
+}
